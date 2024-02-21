167eb77485a6e39c8154c5902c11026dbe32ba9c
soohyeok8901 <soohyeok8901@gmail.com>
2024-02-21 15:42:21 +0900

    Implement webchat timeout

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-const socket = io(`http://localhost:3000/`);
+// const socket = io(`http://localhost:3000/`);
+const socket = io(`https://dev.haze.monster:3001/`);
 
 // 이벤트명 상수들
 const MATCHING_EVENTS = {
@@ -21,6 +22,19 @@ const WEBRTC_EVENTS = {
   OFFER: "offer",
   ANSWER: "answer",
   ICE: "ice",
+};
+
+const WEBCHAT_EVENTS = {
+  LEAVE_WEBCHAT: "leave_webchat",
+  WEBCHAT_ENDED: "webchat_ended",
+  REQUEST_FACE_RECOGNITION: "request_face_recognition",
+  RESPOND_FACE_RECOGNITION: "respond_face_recognition",
+  PERFORM_FACE_RECOGNITION: "perform_face_recognition",
+  FACE_RECOGNITION_REQUEST_DENIED: "face_recognition_request_denied",
+  ALREADY_REQUESTED: "already_requested",
+  RESPOND_IS_TOO_LATE: "respond_is_too_late",
+  REPORT_USER: "report_user",
+  WEBCHAT_TIMEOUT: "webchat_timeout",
 };
 
 // STUN 서버 목록
@@ -55,6 +69,17 @@ const muteButton = document.getElementById("mute");
 const cameraButton = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
+//화상채팅
+const leaveButton = document.getElementById("leave");
+const timeOutButton = document.getElementById("timeOut");
+const faceRecognitionButton = document.getElementById("faceRecognition");
+const faceDiv = document.getElementById("face");
+const faceAcceptButton = document.getElementById("faceAcceptButton");
+const faceDeclineButton = document.getElementById("faceDeclineButton");
+
+//신고
+const reportButton = document.getElementById("report");
+
 
 /**
  * 상태 변수 선언
@@ -68,6 +93,16 @@ let myPeerConnection; // RTCPeerConnection 객체
 let myStream; // 내 영상 stream
 let muted = false; // 내 stream mute 상태
 let cameraOff = false; // 내 stream 카메라 상태
+
+let receivedTime;
+
+// 소개매칭 필터
+const filter = {
+    gender: 'MALE',
+    location: '경기',
+    minAge: 20,
+    maxAge: 25,
+  }
 
 // 초기 실행 함수
 function init() {
@@ -89,6 +124,23 @@ function initSocketListeners() {
   socket.on(MATCHING_EVENTS.NOT_IDLE, handleNotIdle);
   socket.on(MATCHING_EVENTS.NOT_WAITING, handleNotWaiting);
   socket.on(MATCHING_EVENTS.PARTNER_DISCONNECTED, handlePartnerDisconnected);
+  socket.on(WEBCHAT_EVENTS.WEBCHAT_ENDED, handleWebchatEnded);
+
+  // 화상채팅
+  socket.on(
+    WEBCHAT_EVENTS.REQUEST_FACE_RECOGNITION,
+    handleRequestFaceRecognition
+  );
+  socket.on(
+    WEBCHAT_EVENTS.FACE_RECOGNITION_REQUEST_DENIED,
+    handleFaceRecognitionRequestDenied
+  );
+  socket.on(
+    WEBCHAT_EVENTS.PERFORM_FACE_RECOGNITION,
+    handlePerformFaceRecognition
+  );
+  socket.on(WEBCHAT_EVENTS.ALREADY_REQUESTED, handleAlreadyRequested);
+  socket.on(WEBCHAT_EVENTS.RESPOND_IS_TOO_LATE, handleRespondIsTooLate);
 
   // webrtc 이벤트
   socket.on(WEBRTC_EVENTS.START_WEBRTC_SIGNALING, handleStartWebrtcSignaling);
@@ -110,6 +162,22 @@ function initHtmlElementListeners() {
   muteButton.addEventListener("click", handleMuteClick);
   cameraButton.addEventListener("click", handleCameraClick);
   camerasSelect.addEventListener("input", handleCameraChange);
+
+  // 화상채팅 종료 버튼
+  leaveButton.addEventListener("click", handleLeaveClick);
+
+  // 타임아웃 발생 버튼
+  timeOutButton.addEventListener("click", handleTimeOutClick);
+
+  // 얼굴공개 요청 버튼
+  faceRecognitionButton.addEventListener("click", handleFaceRecognitionClick);
+
+  // 얼굴공개 수락/거절버튼
+  faceAcceptButton.addEventListener("click", handleFaceAcceptButtonClick);
+  faceDeclineButton.addEventListener("click", handleFaceDeclineButtonClick);
+
+  // 신고 버튼
+  reportButton.addEventListener("click", handleReportButtonClick);
 }
 
 /**
@@ -117,6 +185,7 @@ function initHtmlElementListeners() {
 */
 call.hidden = true;
 matchedDiv.hidden = true;
+faceDiv.hidden = true;
 
 // 소켓 연결 이벤트 핸들러
 function handleConnect() {
@@ -190,7 +259,9 @@ function handleException(data) {
 function handleRestartMatchingRequest() {
   console.log("다시 매칭을 시도합니다.");
 
-  socket.emit(MATCHING_EVENTS.START_MATCHING, { userId });
+  socket.emit(MATCHING_EVENTS.START_MATCHING, {
+    userId, filter
+  });
 }
 
 // 유저의 상태가 idle이 아님을 알림 이벤트 핸들러
@@ -212,7 +283,50 @@ function handlePartnerDisconnected() {
 
   call.hidden = true; // 화상채팅도중이면 사라지도록 초기화
 
-  socket.emit(MATCHING_EVENTS.START_MATCHING, { userId }); // 소개매칭 재요청
+  socket.emit(MATCHING_EVENTS.START_MATCHING, { userId, filter }); // 소개매칭 재요청
+}
+
+// 화상채팅이 종료됐음을 알림 이벤트 핸들러
+function handleWebchatEnded() {
+  call.hidden = true; // 화상채팅 안보이게
+  matchedDiv.hidden = true; // 상대방 정보 안보이게
+
+  myFace.srcObject = null; // 내 얼굴 스트림 초기화
+  partnerFace.srcObject = null; // 상대방얼굴 스트림 초기화
+
+  myPeerConnection = null; // PeerConnection 초기화
+}
+
+// 화상채팅 도중 얼굴공개 요청이 왔을 때 이벤트 핸들러
+function handleRequestFaceRecognition() {
+  face.hidden = false;
+
+  receivedTime = new Date();
+}
+
+// 얼굴공개 요청이 거부됐을 때
+function handleFaceRecognitionRequestDenied() {
+  console.log("얼굴 공개 거부 됨");
+}
+
+// 얼굴공개 요청이 수락됐을 때
+function handlePerformFaceRecognition() {
+  console.log("얼굴 공개 수락 됨");
+}
+
+// 이미 얼굴공개가 요청됐을 때
+function handleAlreadyRequested() {
+  console.log("이미 얼굴공개 요청된 상태입니다");
+}
+
+// 얼굴공개 요청에 대한 응답이 늦었을 때
+function handleRespondIsTooLate() {
+  console.log("얼굴공개요청에 대한 응답이 늦었습니다");
+}
+
+// 화상채팅도중 상대 유저를 신고할 때
+function handleReportUser() {
+  console.log("얼굴공개요청에 대한 응답이 늦었습니다");
 }
 
 /**
@@ -223,9 +337,9 @@ function onStartMatching(e) {
   console.log("매칭 시작 버튼 클릭");
 
   userId = matchInput.value;
-
+  
   socket.removeEventListener(MATCHING_EVENTS.START_MATCHING, onStartMatching);
-  socket.emit(MATCHING_EVENTS.START_MATCHING, { userId });
+  socket.emit(MATCHING_EVENTS.START_MATCHING, { userId, filter });
 }
 
 function onCancelMatching(e) {
@@ -332,7 +446,7 @@ async function handleStartWebrtcSignaling({ roomName, stunServers }) {
   myPeerConnection.setLocalDescription(offer); // SDP Offer 저장
 
   // offer를 유저 B에게 전송하기 위한 이벤트 발생
-  socket.emit(WEBRTC_EVENTS.OFFER, { offer, roomName });
+  socket.emit(WEBRTC_EVENTS.OFFER, { offer });
 
   console.log("offer 전송 완료");
 
@@ -355,7 +469,7 @@ async function handleOffer({ offer, roomName }) {
   myPeerConnection.setLocalDescription(answer); // answer 저장
 
   // answer를 유저 A에게 전송하기 위한 이벤트 발생
-  socket.emit(WEBRTC_EVENTS.ANSWER, { answer, roomName });
+  socket.emit(WEBRTC_EVENTS.ANSWER, { answer });
 
   console.log("answer 전송 완료");
 };
@@ -399,7 +513,7 @@ async function makeConnection() {
 // 클라이언트가 알아서 수집한 icecandidate 이벤트 핸들러
 function handleIceCandidate(data) {
   // 서로의 클라이언트가 수집한 candidate들을 서로 주고 받는다는 뜻
-  socket.emit(WEBRTC_EVENTS.ICE, { ice: data.candidate, roomName: room });
+  socket.emit(WEBRTC_EVENTS.ICE, { ice: data.candidate });
 
   console.log("candidate 전송 완료");
 }
@@ -455,6 +569,53 @@ async function handleCameraChange() {
   }
 }
 
+// 화상채팅 종료 버튼 클릭 이벤트 핸들러
+function handleLeaveClick() {
+  socket.emit(WEBCHAT_EVENTS.LEAVE_WEBCHAT, { userId });
+}
+
+// 타임아웃 발생 버튼 클릭 이벤트 핸들러
+function handleTimeOutClick() {
+  socket.emit(WEBCHAT_EVENTS.WEBCHAT_TIMEOUT, { userId });
+}
+
+// 얼굴공개요청 버튼 클릭 이벤트 핸들러
+function handleFaceRecognitionClick() {
+  socket.emit(WEBCHAT_EVENTS.REQUEST_FACE_RECOGNITION, { userId });
+}
+
+// 얼굴공개 수락 버튼 클릭 이벤트 핸들러
+function handleFaceAcceptButtonClick() {
+  face.hidden = true;
+
+  console.log(receivedTime)
+
+  socket.emit(WEBCHAT_EVENTS.RESPOND_FACE_RECOGNITION, {
+    userId,
+    response: "accept",
+    receivedTime,
+  });
+}
+
+// 얼굴공개 거절 버튼 클릭 이벤트 핸들러
+function handleFaceDeclineButtonClick() {
+  face.hidden = true;
+
+  socket.emit(WEBCHAT_EVENTS.RESPOND_FACE_RECOGNITION, {
+    userId,
+    response: "decline",
+    receivedTime,
+  });
+}
+
+// 화상채팅도중 상대유저 신고 버튼 클릭 이벤트 핸들러
+function handleReportButtonClick() {
+  face.hidden = true;
+
+  socket.emit(WEBCHAT_EVENTS.REPORT_USER, {
+    userId,
+  });
+}
 
 
 init();
